8ee70c1a12f7094be6ad9728446248b725af9fa9
Goumeida Ahmed Seyf-Eddine <36012779+SeyfGoumeida@users.noreply.github.com>
2020-12-06 17:51:44 +0100

    [EDIT] /articles & README.md

--- src/main/java/com/efrei/CoronaWatch/Controlers/RestWebService.java
+++ src/main/java/com/efrei/CoronaWatch/Controlers/RestWebService.java
@@ -21,12 +21,12 @@ public class RestWebService {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping("/articles")
     public Iterable<Article> getArtticles(){
         return articleRepository.findAll();
     }
 
-    @PostMapping("/")
+    @PostMapping("/articles")
     @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
     public void addArticle(@RequestBody Article article) throws Exception {
         articleRepository.save(article);
