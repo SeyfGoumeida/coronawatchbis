3af401f8ff9d0848c8a7c3621e776f49de305637
Erifai <37931843+Erifai@users.noreply.github.com>
2020-12-21 15:35:47 +0100

    [TRYING] CORS FILTER

--- src/main/java/com/efrei/CoronaWatch/CoronaWatchApplication.java
+++ src/main/java/com/efrei/CoronaWatch/CoronaWatchApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.HashSet;
 import java.util.Iterator;
@@ -24,7 +26,15 @@ public class CoronaWatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaWatchApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 	/*public static void writeJsonSimple() throws Exception {
 		JSONObject sampleObject = new JSONObject();
 		sampleObject.put("name", "Stackabuser");
--- src/main/java/com/efrei/CoronaWatch/Security/WebSecurityConfig.java
+++ src/main/java/com/efrei/CoronaWatch/Security/WebSecurityConfig.java
@@ -9,16 +9,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -55,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Bean
+  /*  @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
@@ -65,8 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
             }
         };
-    }
-    @Override
+    } */
+  /*  @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
@@ -83,9 +77,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
                 cors.setAllowedHeaders(List.of("*"));
                 return cors;
-            });*/
+            });
 
        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+    }*/
 
 }
